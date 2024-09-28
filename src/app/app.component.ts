6fc1c00c766f98dc1f47b587765e74e55bcae167
nicolas <nicolas.castellano.6@hotmail.com>
2024-09-28 12:25:05 -0300

    added routes and reformatted styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'kaijuu-frontend-web';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 // import components to bind to routes
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PostsComponent } from './posts/posts.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
