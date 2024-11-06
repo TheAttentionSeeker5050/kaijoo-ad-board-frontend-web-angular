73db17d2d6b8f9bc412fd7b80980839dce783b96
nicolas <nicolas.castellano.6@hotmail.com>
2024-11-06 11:59:14 -0400

    added category and subcategory dropdowns to post creation and display, along with tag input functionality

--- src/app/add-or-edit-post/add-or-edit-post.component.ts
+++ src/app/add-or-edit-post/add-or-edit-post.component.ts
@@ -12,6 +12,9 @@ import { environment } from '../../environments/environment';
 import { CustomHttpResponseError } from '../../models/CustomHttpResponseError.model';
 import { ImageService } from '../services/Image.service';
 import { CustomNavigationService } from '../services/CustomNavigation.service';
+import { CategoryRelation } from '../../models/Category.model';
+import { SubCategoryRelation } from '../../models/SubCategory.model';
+import { TagRelation } from '../../models/Tag.model';
 
 
 @Component({
@@ -45,6 +48,12 @@ export class AddOrEditPostComponent implements OnInit {
   // To upload the thumbnail
   selectedFile: File | null = null;
 
+  // Categories and subcategories
+  categories: CategoryRelation[] = [];
+  subCategories: SubCategoryRelation[] = [];
+  tags: TagRelation[] = [];
+  selectedTags: TagRelation[] = [];
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -75,6 +84,9 @@ export class AddOrEditPostComponent implements OnInit {
       phone: [''],
       price: [''],
       email: [''],
+      category: [''],
+      subCategory: [''],
+      tags: ['']
     });
 
     // Get the token from the local storage
@@ -104,6 +116,7 @@ export class AddOrEditPostComponent implements OnInit {
             throw new CustomHttpResponseError(response.errorMessage, 200);
           }
 
+
           this.addOrEditPostForm.patchValue({
             title: response.data.title,
             description: response.data.description,
@@ -111,6 +124,16 @@ export class AddOrEditPostComponent implements OnInit {
             price: response.data.price,
             email: response.data.email,
             phone: response.data.phone,
+            category: response.data.category.id,
+            subCategory: response.data.subCategory.id,
+          });
+
+          // Set the selected tags
+          this.selectedTags = response.data.tags.map((tag: any) => {
+            return <TagRelation>{
+              id: tag.id,
+              name: tag.name
+            };
           });
         },
         error: (error) => {
@@ -128,6 +151,79 @@ export class AddOrEditPostComponent implements OnInit {
           this.title = this.postId
             ? `Edit Classified Ad: ${this.addOrEditPostForm.value.title}`
             : 'Add New Classified Ad';
+
+          this.populateCategories();
+          this.onCategoryChange({ target: { value: this.addOrEditPostForm.value.category } });
+        }
+      });
+  }
+
+  populateCategories() {
+    // use http service to get the data from /categories
+    this.httpClientService.get('categories/autocomplete-list')
+      .subscribe({
+        next: (response: any) => {
+          // if errorMessage is present, display it
+          if (response.errorMessage) {
+            // throw an error using custom error class, this is to prevent unwanted errors from being thrown
+            // so only get the error message when this error class is thrown
+            throw new CustomHttpResponseError(response.errorMessage, 200);
+          }
+
+          // Set the categories
+          this.categories = response.data.map((category: any) => {
+            return <CategoryRelation>{
+              id: category.id,
+              name: category.name
+            };
+          });
+        },
+        error: (error) => {
+          // Return the error message depending on the status code
+          if (error.status == 401 || error.status == 403) {
+            this.errorMessage = 'Could not get the categories. Please try again.';
+          } else if (error instanceof CustomHttpResponseError) {
+            this.errorMessage = error.message;
+          } else {
+            this.errorMessage = "Something went wrong. We were unable to get the categories.";
+          }
+        }
+    });
+  }
+
+
+  populateTags(tagFilterName: string = '') {
+    // use http service to get the data from /tags
+    this.httpClientService.get(`tags/autocomplete-list${tagFilterName ? `?name=${tagFilterName}` : ''}`)
+      .subscribe({
+        next: (response: any) => {
+          // if errorMessage is present, display it
+          if (response.errorMessage) {
+            // throw an error using custom error class, this is to prevent unwanted errors from being thrown
+            // so only get the error message when this error class is thrown
+            throw new CustomHttpResponseError(response.errorMessage, 200);
+          }
+
+          // Set the tags
+          this.tags = response.data.map((tag: any) => {
+            return <TagRelation>{
+              id: tag.id,
+              name: tag.name
+            };
+          });
+        },
+        error: (error) => {
+          // Return the error message depending on the status code
+          if (error.status == 401 || error.status == 403) {
+            this.errorMessage = 'Could not get the tags. Please try again.';
+          } else if (error instanceof CustomHttpResponseError) {
+            this.errorMessage = error.message;
+          } else {
+            this.errorMessage = "Something went wrong. We were unable to get the tags.";
+          }
+        }, complete: () => {
+          // the tags that are already selected should not be in the tags list
+          this.tags = this.tags.filter((tag) => !this.selectedTags.find((selectedTag) => selectedTag.id === tag.id));
         }
       });
   }
@@ -147,6 +243,18 @@ export class AddOrEditPostComponent implements OnInit {
         phone: this.addOrEditPostForm.value.phone,
         price: this.addOrEditPostForm.value.price,
         email: this.addOrEditPostForm.value.email,
+        category: {
+          id: this.addOrEditPostForm.value.category
+        },
+        subCategory: {
+          id: this.addOrEditPostForm.value.subCategory
+        },
+        tags: this.selectedTags.map((tag) => {
+          return {
+            id: tag.id,
+            // name: tag.name
+          };
+        })
       };
 
       // send the form data to the server
@@ -164,22 +272,22 @@ export class AddOrEditPostComponent implements OnInit {
             }
 
             // If a file was selected, send the multipart request to upload the thumbnail
-          if (this.selectedFile && this.postId) {
-            const fileData = new FormData();
-            fileData.append('thumbnail', this.selectedFile);
+            if (this.selectedFile && this.postId) {
+              const fileData = new FormData();
+              fileData.append('thumbnail', this.selectedFile);
 
-            this.httpClientService.postMultipart(`posts/by-id/${this.postId}/edit-thumbnail`, fileData, this.token)
-              .subscribe({
-                next: () => {
-                  this.router.navigate(['/posts']);
-                },
-                error: (error) => {
-                  this.handleError(error);
-                }
-              });
-          } else {
-            this.router.navigate(['/posts']);
-          }
+              this.httpClientService.postMultipart(`posts/by-id/${this.postId}/edit-thumbnail`, fileData, this.token)
+                .subscribe({
+                  next: () => {
+                    this.router.navigate(['/posts']);
+                  },
+                  error: (error) => {
+                    this.handleError(error);
+                  }
+                });
+            } else {
+              this.router.navigate(['/posts']);
+            }
           },
           error: (error) => {
             this.handleError(error);
@@ -190,6 +298,44 @@ export class AddOrEditPostComponent implements OnInit {
           }
         });
     }
+  }
+
+  onCategoryChange(event: any) {
+    // Get the id of the selected category
+    const categoryId = event.target.value;
+
+    // Make the request to get the subcategories sub-categories/by-category-id/:id
+    this.httpClientService.get(`sub-categories/by-category-id/${categoryId}`)
+      .subscribe({
+        next: (response: any) => {
+          // if errorMessage is present, display it
+          if (response.errorMessage) {
+            // throw an error using custom error class, this is to prevent unwanted errors from being thrown
+            // so only get the error message when this error class is thrown
+            throw new CustomHttpResponseError(response.errorMessage, 200);
+          }
+
+          // Set the subcategories
+          this.subCategories = response.data.map((subCategory: any) => {
+            return <SubCategoryRelation>{
+              id: subCategory.id,
+              name: subCategory.name
+            };
+          });
+        },
+        error: (error) => {
+          // Return the error message depending on the status code
+          if (error.status == 401 || error.status == 403) {
+            this.errorMessage = 'Could not get the subcategories. Please try again.';
+          } else if (error instanceof CustomHttpResponseError) {
+            this.errorMessage = error.message;
+          } else {
+            this.errorMessage = "Something went wrong. We were unable to get the subcategories.";
+          }
+        },
+        complete: () => {
+        }
+      });
   }
 
   checkCanGoBack() {
@@ -209,6 +355,33 @@ export class AddOrEditPostComponent implements OnInit {
         console.error('Error resizing image:', error);
       });
     }
+  }
+
+  onTagInputChange(event: any) {
+    // use the populate tags function to get the tags
+    this.populateTags(event.target.value);
+  }
+
+  addTag(tag: TagRelation) {
+
+    // Push the tag if tag exist and is not already in the selected tags
+    if (tag && !this.selectedTags.find((selectedTag) => selectedTag.id === tag.id)) {
+      this.selectedTags.push(tag);
+    }
+
+    // clear the input field
+    this.addOrEditPostForm.patchValue({
+      tags: ''
+    });
+
+    // Delete the tag from the tags list
+    this.tags = this.tags.filter((tagItem) => tagItem.id !== tag.id);
+
+  }
+
+  removeTag(tag: number) {
+    // Filter out the tag from the selected tags
+    this.selectedTags = this.selectedTags.filter((selectedTag) => selectedTag.id !== tag);
   }
 
   private handleError(error: any) {
